66411223a67c887477889ecad73ec02d3ee42c55
goodperiodt <ilypsj@gmail.com>
2024-06-11 18:19:39 +0900

    Feat, Modify: Join.js_구조 잡는중

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Reset } from 'styled-reset';
 import './App.css';
-import NewsList from './components/board/NewsList';
+// import NewsList from './components/board/NewsList';
+import Join from './components/user/Join';
 
 function App() {
   return (
     <div className='content-wrapper'>
       <Reset />
-      <NewsList />
+      <Join />
     </div>
   );
 }
