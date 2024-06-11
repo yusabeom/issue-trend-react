4b99b3f018b42ee5c7ee4fda3820899f6c8599ab
Leegayeonn <156872496+Leegayeonn@users.noreply.github.com>
2024-06-11 18:23:03 +0900

    [푸터 컴포넌트 생성, 메인페이지 구현진행중] 헤더, 푸터 틀 구현, 메인페이지 워드클라우드 부분 진행중 (#3)
    Rename: "헤더 module.scss 파일명 변경"
    Feat: "푸터 컴포넌트 구현"
    Feat: "메인페이지 구성중(워드클라우드 페이지 구현 진행)"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import { Reset } from 'styled-reset';
 import './App.css';
 import NewsList from './components/board/NewsList';
+import Header from './common/layout/Header';
+import Footer from './common/layout/Footer';
+import Intro from './components/main/Intro';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import Home from './components/main/Home';
 
 function App() {
   return (
-    <div className='content-wrapper'>
-      <Reset />
-      <NewsList />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Intro />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/newsList' element={<NewsList />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/common/layout/Header.js
+++ src/common/layout/Header.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import logo from '../../assets/img/logo.png';
-import styles from '../../styles/Header.scss';
+import styles from '../../styles/Header.module.scss';
 
 const Header = () => {
+  const {
+    header,
+    headerContainer,
+    headerItem,
+    items,
+    btnGroup,
+    btn,
+    btn1,
+    btn2,
+  } = styles;
+
   return (
-    <header className='header'>
-      <div className='header-container'>
+    <header className={header}>
+      <div className={headerContainer}>
         <img src={logo} height='100' alt='로고이미지' />
-        <div className='header-item'>
-          <div className='items news'>뉴스 </div>
+        <div className={headerItem}>
+          <div className={items}>뉴스 </div>
           <div>|</div>
-          <div className='items board'>게시판 </div>
+          <div className={items}>게시판 </div>
           <div>|</div>
-          <div className='items chat'>실시간</div>
+          <div className={items}>실시간</div>
         </div>
 
-        <div className='btn-group'>
-          <div className='btn btn1'>로그인</div>
-          <div className='btn btn2'>회원가입</div>
+        <div className={btnGroup}>
+          <div className={`${btn} ${btn1}`}>로그인</div>
+          <div className={`${btn} ${btn2}`}>회원가입</div>
         </div>
       </div>
     </header>
