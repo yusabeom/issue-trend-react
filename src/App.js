e5911e59fb7b8659fe78309cb692f1e839b95bb7
lgy <ini03134@naver.com>
2024-06-12 20:30:10 +0900

    Feat: "헤더 스크롤 이벤트 추가 및 전체 글꼴 설정"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
 import NewsList from './components/board/NewsList';
 import Intro from './components/main/Intro';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/main/Home';
 import Join from './components/user/Join';
 import { Reset } from 'styled-reset';
+import './App.css';
 
 function App() {
   return (
