2729b3cb4811712c755e053c7d972e8c8f5958e6
lgy <ini03134@naver.com>
2024-06-12 18:09:28 +0900

    Design: "워드클라우드 scss 수정"

--- src/components/main/WordCloud.js
+++ src/components/main/WordCloud.js
@@ -9,6 +9,7 @@ const WordCloud = () => {
     subheading,
     cloudContainer,
     time,
+    boxContainer,
   } = styles;
   return (
     <>
@@ -17,8 +18,10 @@ const WordCloud = () => {
           <div className={mainTitle}>TODAY</div>
           <div className={subheading}>오늘의 사회뉴스 키워드</div>
         </div>
-        <div className={time}>2024년 06월 11일 18시 기준</div>
-        <div className={cloudContainer}>워드 클라우드</div>
+        <div className={boxContainer}>
+          <div className={time}>2024년 06월 11일 18시 기준</div>
+          <div className={cloudContainer}>워드 클라우드</div>
+        </div>
       </div>
     </>
   );
