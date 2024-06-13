1a7680c8558060fb760dda4ae5d38d67ef02b6c5
goodperiodt <ilypsj@gmail.com>
2024-06-13 18:08:49 +0900

    Modify: 병합 과정중 충돌된 문서 정리

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Home from './components/main/Home';
 import Join from './components/user/Join';
 import './App.css';
 import Layout from './common/layout/Layout';
-
+import NewsList from './components/board/NewsList';
+import ChatModal from './components/chat/ChatModal';
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
         <Route index element={<Intro />} />
         <Route path='/' element={<Layout />}>
           <Route path='/home' element={<Home />} />
-          <Route path='/newsList' element={<NewsList />} />
+          <Route path='/news' element={<NewsList />} />
           <Route path='/join' element={<Join />} />
           <Route path='/news' element={<NewsTemplate />} />
           <Route path='/chat' element={<ChatModal />} />
