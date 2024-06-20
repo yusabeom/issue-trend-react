59b1ea5a506ca7b4e29c299fdebf093fa7bdcaea
lgy <ini03134@naver.com>
2024-06-20 19:01:52 +0900

    마이페이지 구현중

--- src/components/user/mypage/ChangeInfo.js
+++ src/components/user/mypage/ChangeInfo.js
@@ -1,7 +1,86 @@
 import React from 'react';
+import styles from '../../../styles/ChangeInfo.module.scss';
+import { TextField } from '@mui/material';
 
 const ChangeInfo = () => {
-  return <div>ChangeInfo</div>;
+  const { title, content, inputField } = styles;
+  return (
+    <>
+      <div className={title}>내정보 변경</div>
+      <div className={content}>
+        <TextField
+          required
+          id='outlined-required'
+          label='이메일'
+          defaultValue=''
+          className={inputField}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+        <TextField
+          required
+          id='outlined-required'
+          label='현재비밀번호'
+          defaultValue=''
+          className={inputField}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+        <TextField
+          required
+          id='outlined-required'
+          label='비밀번호 변경'
+          defaultValue=''
+          className={inputField}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+        <TextField
+          required
+          id='outlined-required'
+          label='비밀번호 확인'
+          defaultValue=''
+          className={inputField}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+        <TextField
+          required
+          id='outlined-required'
+          label='닉네임'
+          defaultValue=''
+          className={inputField}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+        <TextField
+          required
+          id='outlined-required'
+          label='주소'
+          defaultValue=''
+          className={inputField}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+        <TextField
+          required
+          id='outlined-required'
+          label='관심주제'
+          defaultValue=''
+          className={inputField}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+      </div>
+    </>
+  );
 };
 
 export default ChangeInfo;
