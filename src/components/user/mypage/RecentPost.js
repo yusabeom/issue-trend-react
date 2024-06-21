4aac26a13ac1b7dd62eb8f946e40e1a921f309cd
Leegayeonn <156872496+Leegayeonn@users.noreply.github.com>
2024-06-21 14:15:43 +0900

    [마이페이지] 기본틀완성 (#25)
    * 키워드 그래프 서버 연결완료
    
    * 워드클라우드 클릭이벤트 추가
    
    * 워드클라우드 임포트 삭제
    
    * 설치
    
    * 지역별 뉴스 랜더링 진행중
    
    * 워드클라우드 로딩창 생성(리액트스피너설치)
    
    * 워드클라우드 css 수정
    
    * 지역별 뉴스 수정중
    
    * 지역별 뉴스 클릭이벤트 추가중
    
    * 지역별 뉴스 select scss 변경
    
    * 지역별뉴스모달창 scss 변경
    
    * Feat: "KeywordContext 생성, 워드클라우드 단어 클릭시 뉴스검색페이지 이동"
    
    * 뉴스 키워드 중복되어 검색안되는 현상 해결
    
    * 실시간 검색 서버연결완료
    
    * 뉴스키워드 재수정
    
    * 워드클라우드 캔버스크기조절
    
    * 검색어 시간 추가, 뉴스템플릿 헤더컴포너트 중복되어 삭제
    
    * 마이페이지 틀 구성
    
    * 마이페이지 폴더생성
    
    * 마이페이지 구현중
    
    * [마이페이지] 내정보변경페이지 틀구현
    
    * 마이페이지 틀 완성

--- src/components/user/mypage/RecentPost.js
+++ src/components/user/mypage/RecentPost.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import styles from '../../../styles/RecentPost.module.scss';
 
+import img from '../../../assets/img/news3.jpg';
+
+const { title, post, postContent } = styles;
 const RecentPost = () => {
-  return <div>RecentPost</div>;
+  return (
+    <>
+      <div className={title}>최근의 본글</div>
+      <div className={post}>
+        <img src={img} />
+        <ul className={postContent}>
+          <li>제목</li>
+          <li>내용</li>
+        </ul>
+      </div>
+      <div className={post}>
+        <img src={img} />
+        <ul className={postContent}>
+          <li>제목</li>
+          <li>내용</li>
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default RecentPost;
