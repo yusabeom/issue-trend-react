1a711e795395e86c0c715d983c527a9002481cb1
jeongwon <tjtkdvl@gmail.com>
2024-06-23 16:37:43 +0900

    Feat: 게시물 상세페이지 구현 중

--- src/components/board/ReportDetail.js
+++ src/components/board/ReportDetail.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams, useSearchParams } from 'react-router-dom';
+import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import styles from '../../styles/ReportDetail.module.scss';
 import { Button } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -16,11 +16,21 @@ const ARTICLE = API_BASE_URL + USER;
 
 const ReportDetail = () => {
   const [openReply, setOpenReply] = useState(false); // 댓글창 열기
-  const [boardDetail, setBoardDetail] = useState(null); // 게시물 정보
+  const [boardDetail, setBoardDetail] = useState({
+    postNo: 0,
+    userNo: 0,
+    title: '',
+    text: '',
+    writeDate: '',
+    img: '',
+    email: '',
+    formatDate: '',
+  }); // 게시물 정보
   const [writerProfile, setWriterProfile] = useState(
     'https://i.namu.wiki/i/GQMqb8jtiqpCo6_US7jmWDO30KfPB2MMvbdURVub61Rs6ALKqbG-nUATj-wNk7bXXWIDjiLHJxWYkTELUgybkA.webp',
   ); // 작성자 프사
   const [replyList, setReplyList] = useState([]); // 댓글 리스트
+  const [imgUrl, setImgUrl] = useState(''); // 게시글 첨부 이미지
 
   // 경로 상에 붙은 변수 정보(path variable)을 가져오는 방법
   // ex) /board/detail/{data}
@@ -29,41 +39,51 @@ const ReportDetail = () => {
   // 요청과 함께 전달된 쿼리스트링을 가져오는 방법.
   // ex) /board/list?page=2&size=10
   const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
 
   const page = searchParams.get('page') || 1;
   const size = searchParams.get('size') || 20;
 
-  useEffect(() => {
-    /*
-    // (path variabled인 id에 따라) 서버로부터 게시물 데이터 가져오기
-    // 게시물 정보의 writer 정보도 같이 (프사를 위해)
-    // 게시물 정보의 reply 정보도 같이 (댓글 정보)
-    // fetch가 정상적으로 이루어지면 loading을 false로
-    const res = await axios.get(REPORT_DETAIL_URL, { id });
+  // 게시물 상세페이지 불러오기
+  const fetchData = async () => {
+    try {
+      console.log('GET 요청 url: ', ARTICLE + '/search-post/' + id);
+      const res = await axios.get(ARTICLE + '/search-post/' + id);
+      const getBoardDetail = await res.data;
 
-    const getBoardDetail = await res.data; // 게시물 데이터
-    */
+      console.log(getBoardDetail.postNo);
+      setBoardDetail(getBoardDetail);
+      console.log('set실행: ', boardDetail);
+    } catch (error) {
+      // console.error('Error fetching data: ', error);
+      console.error(error);
+    }
+  };
 
-    const fetchData = async () => {
-      try {
-        console.log('GET 요청 url: ', ARTICLE + '/search-post/' + id);
-        const res = await axios.get(ARTICLE + '/search-post/' + id);
-        const getBoardDetail = await res.data;
+  // 게시물 이미지 불러오기
+  const fetchImage = async () => {
+    try {
+      console.log(
+        'GET 요청 url: ',
+        ARTICLE + '/load-image/' + boardDetail.postNo,
+      );
+      const res = await axios.get(
+        ARTICLE + '/load-image/' + boardDetail.postNo,
+      );
+      const getBoardImg = await res.data;
 
-        setBoardDetail(getBoardDetail);
-      } catch (error) {
-        // console.error('Error fetching data: ', error);
-        console.error(error);
-      }
-    };
+      setImgUrl(getBoardImg);
+      // console.log('imgUrl: ', imgUrl);
+    } catch (error) {
+      // console.error('Error fetching data: ', error);
+      console.error(error);
+    }
+  };
 
-    fetchData();
-  }, []);
-
+  // 해당 게시글 댓글 불러오기
   const bringReplies = () => {
-    console.log('boardDetail: ', boardDetail);
+    console.log('boardDetail: ', boardDetail.postNo);
     const fetchRepliesData = async () => {
-      // 해당 게시글 댓글 불러오기
       // "/articles/{postNo}/comments"
       console.log(
         'GET 요청 url: ',
@@ -82,7 +102,14 @@ const ReportDetail = () => {
   };
 
   useEffect(() => {
+    fetchData();
+    console.log('postNo is ', boardDetail.postNo);
+    fetchImage();
     bringReplies();
+  }, []);
+
+  useEffect(() => {
+    // console.log('postNo is ', boardDetail.postNo);
   }, [boardDetail]);
 
   // 댓글 리스트 열기
@@ -114,7 +141,14 @@ const ReportDetail = () => {
 
   return (
     <div className={`aspect-ratio ${styles.wrapper}`}>
-      <h2>제보 게시판</h2>
+      <h2
+        style={{ cursor: 'pointer' }}
+        onClick={() => {
+          navigate('/board');
+        }}
+      >
+        제보 게시판
+      </h2>
       {boardDetail && (
         <div className={styles.board}>
           <header className='boardHeader'>
@@ -127,12 +161,12 @@ const ReportDetail = () => {
             </div>
 
             <div>
-              <p className={styles.writer}>
+              <div className={styles.writer}>
                 <div className={styles.profile}>
                   <img src={writerProfile} alt='작성자 프로필 사진' />
                 </div>
                 {boardDetail.email}
-              </p>
+              </div>
               <div>
                 <a href=''>신고하기</a>
                 <p>{boardDetail.formatDate}</p>
@@ -149,6 +183,9 @@ const ReportDetail = () => {
               <div className={styles.tag}>#제보</div>
               <div className={styles.tag}>#경찰</div>
             </div> */}
+            <div>
+              <img src={imgUrl} alt='게시물 이미지' />
+            </div>
           </main>
 
           <hr />
